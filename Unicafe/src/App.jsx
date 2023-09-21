9e6f372301186c99152318d07e45442fc507328c
Aidan van Geest <aidanvangeest@gmail.com>
2023-09-21 16:27:01 +0200

    unicafe step2&3

--- Unicafe/src/App.jsx
+++ Unicafe/src/App.jsx
@@ -1,22 +1,27 @@
 import { useState } from 'react'
 import Button from './Button';
+import Statistics from './Statistics';
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
 
   const handleGoodClick = () => {
     setGood(good + 1);
+    setAll(all + 1);
   }
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1);
+    setAll(all + 1);
   }
 
   const handleBadClick = () => {
     setBad(bad + 1);
+    setAll(all + 1);
   }
 
   return (
@@ -25,11 +30,8 @@ const App = () => {
       <Button handleClick={handleGoodClick} text="good"/>
       <Button handleClick={handleNeutralClick} text="neutral"/>
       <Button handleClick={handleBadClick} text="bad"/>
-
-      <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} />
+      
     </div>
   )
 }
