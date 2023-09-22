09e507ae8e990f9a6d734b6739f7154429411785
Aidan van Geest <aidanvangeest@gmail.com>
2023-09-22 13:33:17 +0200

    unicafe step5

---
+++ Unicafe/src/StatisticLine.jsx
@@ -0,0 +1,10 @@
+const StatisticLine = ({text, value}) => {
+    if (text === "positive") {
+        return <p>{text} {value}%</p>
+    }
+
+    return(
+        <p>{text} {value}</p>
+    )
+}
+export default StatisticLine;
--- Unicafe/src/Statistics.jsx
+++ Unicafe/src/Statistics.jsx
@@ -1,15 +1,17 @@
+import StatisticLine from "./StatisticLine";
+
 const Statistics = ({good, neutral, bad, all}) => {
     if (all === 0) {
         return <p>No feedback given</p>
     }
     return (
         <div>
-            <p>good {good}</p>
-            <p>neutral {neutral}</p>
-            <p>bad {bad}</p>
-            <p>all {all}</p>
-            <p>average {(good-bad)/all}</p>
-            <p>positive {(good/all)*100}%</p>
+            <StatisticLine text="good" value={good} />
+            <StatisticLine text="neutral" value={neutral} />
+            <StatisticLine text="bad" value={bad} />
+            <StatisticLine text="all" value={all} />
+            <StatisticLine text="average" value={(good-bad)/all} />
+            <StatisticLine text="positive" value={(good/all)*100} />
         </div>
     )
 }
